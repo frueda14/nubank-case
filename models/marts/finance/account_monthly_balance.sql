4131108cfd5747e21353650719c4e5759e48f8a9
Profile Name Here <fabian.rueda14@gmail.com>
2025-05-04 13:57:52 +0000

    Creating account_monthly_balance final model

--- models/marts/finance/account_monthly_balance.sql
+++ models/marts/finance/account_monthly_balance.sql
@@ -1,1 +1,30 @@
--- TODO: final metric logic
+{{ 
+    config(materialized='incremental',
+           unique_key='account_id||month',
+           incremental_strategy='merge'
+          ) 
+}}
+
+with movements as (
+    select
+        account_id,
+        date_trunc('month', txn_ts) as month,
+        sum(signed_amount)          as delta_month
+    from {{ ref('int_transactions_union') }}
+    where txn_ts is not null
+    group by 1, 2
+),
+
+running as (
+    select
+        account_id,
+        month,
+        sum(delta_month) over (
+            partition by account_id
+            order by month
+            rows between unbounded preceding and current row
+        ) as closing_balance
+    from movements
+)
+
+select * from running
